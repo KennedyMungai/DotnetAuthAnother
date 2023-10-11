98cd676c7321586e4ba6b8aa7ba1953372bdf006
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-11 12:53:54 +0300

    Added the endpoints to read data from the database

--- DotnetAuthAnother.Api/Controllers/TeamController.cs
+++ DotnetAuthAnother.Api/Controllers/TeamController.cs
@@ -1,3 +1,5 @@
+using DotnetAuthAnother.Api.Models.Dtos;
+using DotnetAuthAnother.Api.Repositories.TeamsRepositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetAuthAnother.Api.Controllers;
@@ -5,7 +7,33 @@ namespace DotnetAuthAnother.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class WeatherForecastController : ControllerBase
+public class TeamsController : ControllerBase
 {
+    private readonly ITeamsCrudService _teamsCrudService;
 
+    public TeamsController(ITeamsCrudService teamsCrudService)
+    {
+        _teamsCrudService = teamsCrudService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ReadTeamsDataDto>>> GetAllTeams([FromQuery] int page, [FromQuery] int pageSize)
+    {
+        var teams = _teamsCrudService.GetAllTeams(page, pageSize);
+
+        return await Task.FromResult(Ok(teams));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ReadTeamsDataDto>> GetTeamById(int id)
+    {
+        var team = _teamsCrudService.GetTeamById(id);
+
+        if (team is null)
+        {
+            return await Task.FromResult(NotFound("The team with id " + id + "was not found"));
+        }
+
+        return await Task.FromResult(Ok(team));
+    }
 }
