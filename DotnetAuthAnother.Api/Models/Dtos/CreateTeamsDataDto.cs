be235baf8f6f520dfb4564908dc20fa7d8279063
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-11 13:08:50 +0300

    Added the Id property to the Dto

--- DotnetAuthAnother.Api/Models/Dtos/CreateTeamsDataDto.cs
+++ DotnetAuthAnother.Api/Models/Dtos/CreateTeamsDataDto.cs
@@ -3,6 +3,7 @@ namespace DotnetAuthAnother.Api.Models.Dtos;
 
 public class CreateTeamsDataDto
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Country { get; set; } = string.Empty;
     public string TeamPrinciple { get; set; } = string.Empty;
