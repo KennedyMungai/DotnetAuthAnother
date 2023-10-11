367fa61fe22af33a7188428b5c7d795034db18cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-11 11:43:43 +0300

    Created the TeamController file

---
+++ DotnetAuthAnother.Api/Controllers/TeamController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAuthAnother.Api.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+
+}
