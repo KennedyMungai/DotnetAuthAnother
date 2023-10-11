778864b425b94c5ef8451684aa22ea37ad66f932
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-11 11:57:05 +0300

    Added some code to generate 10000 records for the teams data

--- DotnetAuthAnother.Api/Data/TeamsDbContext.cs
+++ DotnetAuthAnother.Api/Data/TeamsDbContext.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using DotnetAuthAnother.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,4 +12,19 @@ public class TeamsDbContext : DbContext
     }
 
     public DbSet<TeamModel> TeamModels { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var id = 1;
+
+        var teams = new Faker<TeamModel>()
+                            .RuleFor(m => m.Id, f => id++)
+                            .RuleFor(m => m.Name, f => f.Company.CompanyName())
+                            .RuleFor(m => m.Country, f => f.Address.Country())
+                            .RuleFor(m => m.TeamPrinciple, f => f.Lorem.Paragraphs(1));
+
+        modelBuilder
+            .Entity<TeamModel>()
+            .HasData(teams.Generate(10000));
+    }
 }
