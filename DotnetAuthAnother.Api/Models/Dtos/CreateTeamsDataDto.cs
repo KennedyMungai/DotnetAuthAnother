2624cdd90a0b81611ae7cf284a55e3def8b8f2b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-11 12:19:12 +0300

    Created the Dto for Creating entries to the database

---
+++ DotnetAuthAnother.Api/Models/Dtos/CreateTeamsDataDto.cs
@@ -0,0 +1,9 @@
+namespace DotnetAuthAnother.Api.Models.Dtos;
+
+
+public class CreateTeamsDataDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string TeamPrinciple { get; set; } = string.Empty;
+}
